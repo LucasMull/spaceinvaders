91b5b965b36cb4992d167d39df87425f04b9e7b1
LucasMull <muller.lucas@hotmail.com>
2019-11-07 23:00:12 -0200

    Implementado todos os movimentos

--- listaencadeada.c
+++ listaencadeada.c
@@ -1,11 +1,6 @@
 /*	COISAS A FAZER:
- *		AJUSTAR POSX e POSY DA MOSHIP
- *		AUMENTAR BORDA E DIMINUIR ROW DO TANQUE
- *		CRIAR UMA JANELA SEPARADA PARA O TANQUE E OS TIROS
- *
- *
- *
- *
+ *		CRIAR UMA JANELA SEPARADA OS TIROS E MOTHERSHIP
+ *		IMPLEMENTAR BORDA SEM CAUSAR FLICKER
  */
 
 
@@ -34,13 +29,19 @@
 #define TANK1 "\n /^\\ \nMMMMM\0"
 
 #define MOSHIP1 "\n /MMMMM\\ \nAMoMoMoMA\n \\/'-'\\/ \0"
-#define MOSHIP2 "\n /MMMMM\\ \nMoMoMAAMo\n \\/'-'\\/ \0"
+#define MOSHIP2 "\n /MMMMM\\ \nAoMoMoMoA\n \\/'-'\\/ \0"
 
+#define COR_BORDA 0
+#define COR_NAVE 1
+#define COR_TANQUE 2
+#define COR_TIRO_NAVE 3
+#define COR_TIRO_TANQUE 4
+#define COR_MOSHIP 5
 
 struct t_node
 {
  char *sprite1;
- char *sprite2;
+ char *sprite2; /*<- ignorado para elementos com só 1 sprite*/
  int posx;
  int posy;
  struct  t_node *prox;
@@ -50,17 +51,29 @@ struct t_node
 struct t_lista
 {
  t_node *atual;
- 
+
  t_node *ini[5]; /*sentinela no ini de cada row*/
  t_node *fim[5]; /*sentinela no fim de cada row*/
+
  t_node *tanque; /*tanque no inicio da lista*/
  t_node *moship; /*nave mãe no fim da lista*/
 
  int updateField; /*controla temporização*/
  char direcao; /*alterna dir de mov das naves*/
  int qtd_naves;
- int vel_naves;
+ 
 }; typedef struct t_lista t_lista;
+
+struct t_win /* para facilitar na hora de chamar janela nas funções */
+{
+ WINDOW *enemy;
+ WINDOW *moship;
+ WINDOW *enemyFire;
+
+ WINDOW *tank;
+ WINDOW *tankFire;
+}; typedef struct t_win t_win;
+
 
 int inicializaLista (t_lista *l)
 {
@@ -127,7 +140,7 @@ int inicializaLista (t_lista *l)
 int inicializaNaves (t_lista *l)
 {
  t_node* new_ship;
- int posRow = 8, posCol = 4;
+ int posRow = 8, posCol = 5;
  int i, naves = 0;
  
  l->moship->sprite1 = MOSHIP1;
@@ -135,7 +148,11 @@ int inicializaNaves (t_lista *l)
  l->tanque->sprite1 = TANK1;
 
  l->tanque->posx = COL/2;
- l->tanque->posy = 34;
+ l->tanque->posy = 0; /*posição é 0, porém a window do tanque foi deslocada para baixo das naves*/
+ 
+ l->moship->posx = COL/2;
+ l->moship->posy = 1;
+
  for ( i=0; i<5; i++ )
  {
  	while ( ++naves % 12 != 0 ) /*garante que seja printado 55 naves, 11 por row*/
@@ -149,7 +166,8 @@ int inicializaNaves (t_lista *l)
 		l->fim[i]->prev->prox = new_ship;
 		l->fim[i]->prev = new_ship;
 		
-		new_ship->posx = posRow += 8;	
+		new_ship->posx = posRow;
+		posRow += 8;	
 		new_ship->posy = posCol;
 		switch (i)
 		{
@@ -180,13 +198,10 @@ int inicializaNaves (t_lista *l)
  return 1;
 }
 
-void printNaves(t_lista *l, WINDOW *win)
+void printTanque(t_lista *l, WINDOW *win)
 {
-	int i, j; /*contadores*/
-	int col, row; /*para formatação dos chars da nave*/
+	int row, col, j;
 
-	/* TEMPORARIO APENAS PARA VISUALIZACAO DA MOSHIP 
-	 * CRIAR FÇ DEPOIS */
 	row=0; col=-2; j=0;
 	while ( l->tanque->sprite1[j] != '\0' )
 	{
@@ -197,13 +212,22 @@ void printNaves(t_lista *l, WINDOW *win)
 		}
 		else
 		{
-				wmove(win, l->tanque->posy+row, l->tanque->posx+col);
-				waddch(win, l->tanque->sprite1[j]);
+			wmove(win, l->tanque->posy+row, l->tanque->posx+col);
+			waddch(win, l->tanque->sprite1[j]);
 		}
 		col++;
 		j++;
 	}
+	wrefresh(win);
+}
 
+void printNaves(t_lista *l, t_win *win)
+{
+	int i, j; /*contadores*/
+	int col, row; /*para formatação dos chars da nave*/
+
+	/* TEMPORARIO APENAS PARA VISUALIZACAO DA MOSHIP 
+	 * CRIAR FÇ DEPOIS */
 	row=0; col=-4; j=0;
 	while ( l->moship->sprite1[j] != '\0' )
 	{
@@ -213,14 +237,21 @@ void printNaves(t_lista *l, WINDOW *win)
 			col=-4;
 		}
 		else
-		{
-				wmove(win, row, col+COL/2);
-				waddch(win, l->moship->sprite1[j]);
+		{	
+			if ( l->moship->posx % 2 == 0 )
+			{
+				wmove(win->moship, l->moship->posy+row, l->moship->posx+col);
+				waddch(win->moship, l->moship->sprite1[j]);
+			}
+			else
+			{
+				wmove(win->moship, l->moship->posy+row, l->moship->posx+col);
+				waddch(win->moship, l->moship->sprite2[j]);
+			}
 		}
 		col++;
 		j++;
 	}
-
 
 	for ( i=0; i<5; i++ )
 	{
@@ -237,34 +268,68 @@ void printNaves(t_lista *l, WINDOW *win)
 			}
 			else
 			{
-				if ( l->atual->posx % 2 == 0 ) /* animação do sprite das naves */
+				if ( l->atual->posx % 2 == 0 ) /* animação do sprite das naves alterna em posx par e impar*/
 				{
-					wmove(win, l->atual->posy+row, l->atual->posx+col);
-					waddch(win, l->atual->sprite1[j]);
+					wmove(win->enemy, l->atual->posy+row, l->atual->posx+col);
+					waddch(win->enemy, l->atual->sprite1[j]);
 				}
 				else
 				{
-					wmove(win, l->atual->posy+row, l->atual->posx+col);
-					waddch(win, l->atual->sprite2[j]);
+					wmove(win->enemy, l->atual->posy+row, l->atual->posx+col);
+					waddch(win->enemy, l->atual->sprite2[j]);
 				}
 			}
 			col++;
 			j++;
 		  }
 		  l->atual = l->atual->prox;
-		  wrefresh(win);
 		}
 	}
+	wrefresh(win->enemy);
+	wrefresh(win->moship);
+}
+
+void desceNaves(t_lista *l, WINDOW *win)
+{
+	t_node *aux;
+
+	aux = l->fim[4]->prev;
+	while ( aux != l->ini[0] )
+	{
+		if ( aux->posy+1 < ROW-6 )
+		{
+			aux->posy++;
+			aux = aux->prev;
+		}
+		else /* naves descem no row do tanque e gameover */
+		{
+			endwin();
+			exit(1);
+		}
+	}
+}
+
+void movimentaMoShip(t_lista *l, WINDOW *win)
+{
+	if ( l->moship->posx+6 < COL )
+		l->moship->posx++;
+	else
+		l->moship->posx = 5;
+	wclear(win);
 }
 
 void movimentaNaves(t_lista *l, WINDOW *win)
 {
 	int i;
+	int SWITCH = FALSE; /*para controlar saída do nested loop*/ 
 
 	if ( l->direcao == RIGHT )
 	{
 		for ( i=0; i<5; i++ )
 		{
+			if ( SWITCH == TRUE )
+				break;
+			
 			l->atual = l->fim[i]->prev;
 			while ( l->atual != l->ini[i] )
 			{
@@ -273,6 +338,9 @@ void movimentaNaves(t_lista *l, WINDOW *win)
 				else
 				{
 					l->direcao = LEFT;
+					desceNaves(l, win);
+					
+					SWITCH = TRUE; /*força saída do nested loop*/
 					break;
 				}
 
@@ -280,11 +348,13 @@ void movimentaNaves(t_lista *l, WINDOW *win)
 			}
 		}
 	}
-	
-	else
+	else/*if ( l->direcao == LEFT ) */
 	{
 		for ( i=4; i>=0; i-- )
 		{
+			if ( SWITCH == TRUE )
+				break;
+			
 			l->atual = l->ini[i]->prox;
 			while ( l->atual != l->fim[i] )
 			{
@@ -293,6 +363,9 @@ void movimentaNaves(t_lista *l, WINDOW *win)
 				else
 				{
 					l->direcao = RIGHT;
+					desceNaves(l, win);
+					
+					SWITCH = TRUE; /*força saída do nested loop*/
 					break;
 				}
 
@@ -301,48 +374,65 @@ void movimentaNaves(t_lista *l, WINDOW *win)
 		}
 
 	}
+	
+	wclear(win);
 }
 
 void movimentaTanque(t_lista *l, WINDOW *win, int key)
 {
 	switch (key)
 	{
-		case ' ':
+		case ' ': /* BOTÃO DE TIRO */
 			break;
-		case 'a':
+		case 'a': /* BOTÃO DE ANDAR PARA ESQUERDA */
 			if ( l->tanque->posx - 1 > 2 )
 				l->tanque->posx--;
+			wclear(win);
 			break;
-		case 'd':
+		case 'd': /* BOTÃO DE ANDAR PARA DIREITA */
 			if ( l->tanque->posx + 1 < COL-5 )
 				l->tanque->posx++;
+			wclear(win);
 			break;	
-		case 'q':
+		case 'q': /* BOTÃO DE EXIT */
 			endwin();
 			exit(1);
+		case 'p': /* BOTÃO DE PAUSE */
+			key = 0;
+			while ( (key=getch()) != 'p')
+			{
+				if ( key == 'q' ) /* PARA PODER SAIR DO JOGO DURANTE O PAUSE */
+				{
+					endwin();
+					exit(1);
+				}
+				usleep(10000);
+			}
+			break;
 		default:
 			break;
 	}
-
 }
 
-int GameOn(t_lista *l, WINDOW *win)
+int GameOn(t_lista *l, t_win *win)
 {
 	int key;
 	int utime = 10000;
-	
-	if ( l->updateField % 60000 == 0 )
-	{
-		wclear(win);
-		box(win, 0, 0);
+ 
+	if ( l->updateField % 70000 == 0 )
+		movimentaMoShip(l, win->moship);
+
+	if ( l->updateField % 100000 == 0 )
+	{	
+		printTanque(l, win->tank);
 		printNaves(l, win);
 	}
-	
+
 	key = getch();
-	movimentaTanque(l, win, key);
+	movimentaTanque(l, win->tank, key);
 	if ( l->updateField % 300000 == 0 )
 	{
-		movimentaNaves(l, win);
+		movimentaNaves(l, win->enemy);
 		l->updateField = 0;
 	}
 
@@ -354,6 +444,18 @@ int GameOn(t_lista *l, WINDOW *win)
 	return 1;
 }
 
+void borda(int y1, int x1, int y2, int x2)
+{
+    mvhline(y1, x1, 0, x2-x1);
+    mvhline(y2, x1, 0, x2-x1);
+    mvvline(y1, x1, 0, y2-y1);
+    mvvline(y1, x2, 0, y2-y1);
+    mvaddch(y1, x1, ACS_ULCORNER);
+    mvaddch(y2, x1, ACS_LLCORNER);
+    mvaddch(y1, x2, ACS_URCORNER);
+    mvaddch(y2, x2, ACS_LRCORNER);
+}
+
 int main()
 {
 /* START ncurse */
@@ -362,12 +464,18 @@ int main()
   noecho();
   curs_set(FALSE);
   nodelay(stdscr, TRUE);
+  keypad(stdscr, TRUE);
+  start_color();
 /* WINDOW properties */
   int gety, getx;
   int key;
 /* GAME properties */
   t_lista l;
   	l.updateField = 0;
+  t_win win;
+	win.moship = newwin(5,COL,1,1);
+  	win.enemy = newwin(ROW-3,COL,1,1);
+  	win.tank = newwin(3,COL,ROW-3,1);
 
   getmaxyx(stdscr, gety, getx);
   while (key != 'a')
@@ -391,14 +499,23 @@ int main()
   }
   clear();
 
-  WINDOW *enemy_win = newwin(ROW, COL, 1, 1);
-  refresh();
-  keypad(enemy_win, TRUE);
+  init_pair(COR_NAVE, COLOR_GREEN, COLOR_BLACK);
+  wattron(win.enemy, COLOR_PAIR(COR_NAVE));
+  wattron(win.enemy, A_BOLD);
+  
+  init_pair(COR_TANQUE, COLOR_MAGENTA, COLOR_BLACK);
+  wattron(win.tank, COLOR_PAIR(COR_TANQUE));
+  wattron(win.tank, A_BOLD);
+ 
+  init_pair(COR_MOSHIP, COLOR_RED, COLOR_BLACK);
+  wattron(win.moship, COLOR_PAIR(COR_MOSHIP));
+  wattron(win.moship, A_BOLD);
+  /*borda(0, 0, ROW, COL+1);*/
   
   inicializaLista(&l);
   inicializaNaves(&l);
   
-  while (GameOn(&l, enemy_win));
+  while (GameOn(&l, &win));
   sleep(2);
   
   endwin();
